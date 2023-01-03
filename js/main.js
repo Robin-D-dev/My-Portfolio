050108224e10093ede313b395d688c05484f6dde
Robin <robin.aeroapj@gmail.com>
2023-01-03 17:49:53 +0530

    resume added

--- js/main.js
+++ js/main.js
@@ -1,117 +1,124 @@
 (function ($) {
-    "use strict";
+	"use strict";
 
-    // Navbar on scrolling
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.navbar').fadeIn('slow').css('display', 'flex');
-        } else {
-            $('.navbar').fadeOut('slow').css('display', 'none');
-        }
-    });
-
-
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
+	// Navbar on scrolling
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 200) {
+			$('.navbar').fadeIn('slow').css('display', 'flex');
+		} else {
+			$('.navbar').fadeOut('slow').css('display', 'none');
+		}
+	});
 
 
-    // Typed Initiate
-    if ($('.typed-text-output').length == 1) {
-        var typed_strings = $('.typed-text').text();
-        var typed = new Typed('.typed-text-output', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        });
-    }
+	// Smooth scrolling on the navbar links
+	$(".navbar-nav a").on('click', function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $(this.hash).offset().top - 45
+			}, 1500, 'easeInOutExpo');
+
+			if ($(this).parents('.navbar-nav').length) {
+				$('.navbar-nav .active').removeClass('active');
+				$(this).closest('a').addClass('active');
+			}
+		}
+	});
 
 
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
-
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
+	// Typed Initiate
+	if ($('.typed-text-output').length == 1) {
+		var typed_strings = $('.typed-text').text();
+		var typed = new Typed('.typed-text-output', {
+			strings: typed_strings.split(', '),
+			typeSpeed: 100,
+			backSpeed: 20,
+			smartBackspace: false,
+			loop: true
+		});
+	}
 
 
-    // Scroll to Bottom
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.scroll-to-bottom').fadeOut('slow');
-        } else {
-            $('.scroll-to-bottom').fadeIn('slow');
-        }
-    });
+	// Modal Video
+	$(document).ready(function () {
+		var $videoSrc;
+		$('.btn-play').click(function () {
+			$videoSrc = $(this).data("src");
+		});
+		console.log($videoSrc);
+
+		$('#videoModal').on('shown.bs.modal', function (e) {
+			$("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+		})
+
+		$('#videoModal').on('hide.bs.modal', function (e) {
+			$("#video").attr('src', $videoSrc);
+		})
+	});
 
 
-    // Skills
-    $('.skill').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
+	// Scroll to Bottom
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.scroll-to-bottom').fadeOut('slow');
+		} else {
+			$('.scroll-to-bottom').fadeIn('slow');
+		}
+	});
 
 
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+	// Skills
+	$('.skill').waypoint(function () {
+		$('.progress .progress-bar').each(function () {
+			$(this).css("width", $(this).attr("aria-valuenow") + '%');
+		});
+	}, { offset: '80%' });
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        dots: true,
-        loop: true,
-        items: 1
-    });
-    
+	// Portfolio isotope and filter
+	var portfolioIsotope = $('.portfolio-container').isotope({
+		itemSelector: '.portfolio-item',
+		layoutMode: 'fitRows'
+	});
+	$('#portfolio-flters li').on('click', function () {
+		$("#portfolio-flters li").removeClass('active');
+		$(this).addClass('active');
+
+		portfolioIsotope.isotope({ filter: $(this).data('filter') });
+	});
+
+
+	// Back to top button
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 200) {
+			$('.back-to-top').fadeIn('slow');
+		} else {
+			$('.back-to-top').fadeOut('slow');
+		}
+	});
+	$('.back-to-top').click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+		return false;
+	});
+
+
+	// Testimonials carousel
+	$(".testimonial-carousel").owlCarousel({
+		autoplay: true,
+		smartSpeed: 1500,
+		dots: true,
+		loop: true,
+		items: 1
+	});
+
 })(jQuery);
 
+// const download = document.getElementById('download-cv');
+
+// download.addEventListener('click', request);
+
+// function request() {
+// 	window.location = 'resume.docx';
+// }
